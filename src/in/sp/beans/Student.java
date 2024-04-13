470c9921219581c286f85a1115b82152673b98ed
Yash Batavle <yashbatavle25y@gmail.com>
2024-04-13 21:39:22 +0530

    Autowiring of 3 bean object using java config

---
+++ src/in/sp/beans/Student.java
@@ -0,0 +1,42 @@
+package in.sp.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Student {
+	private int rollno;
+	private String name;
+
+	@Autowired
+	private Address address;
+	@Autowired
+	private Subjects subjects;
+
+	public int getRollno() {
+		return rollno;
+	}
+
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public void display() {
+		System.out.println("Rollno : " + rollno);
+		System.out.println("Name : " + name);
+		System.out.println("Address : " + address);
+		System.out.println("Subject1 :" + subjects);
+//		System.out.println("subject2 :" +);
+//		System.out.println("Subject3 :" + );
+//		System.out.println("subject4 :" + );
+	}
+	
+}
